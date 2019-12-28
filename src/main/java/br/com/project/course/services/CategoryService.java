ba5a2cff2fb79afb222bd890a3edf3fdd9047e5c
wallisonmoura <wallison_cedaspy@hotmail.com>
2019-12-28 12:16:35 -0300

    projeto

---
+++ src/main/java/br/com/project/course/services/CategoryService.java
@@ -0,0 +1,28 @@
+package br.com.project.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.project.course.entities.Category;
+import br.com.project.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
